6d6511af79464a0d03ef935f28ec06510b409762
Shedrack <shedrackejeh458@gmail.com>
2021-03-20 10:01:26 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Contact from './component/Contacts'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  state = {
+    contacts: []
+  }
+  componentDidMount() {
+    fetch('http://jsonplaceholder.typicode.com/users').then(res => res.json).then((data) => {
+      this.setState({contacts: data})
+    }).catch(console.log)
+  }
+  render() {
+    return (
+      <div>
+        <Contact contacts={ this.state.contacts}/>
+      </div>
+    )
+  }
+};
 
-export default App;
+export default App
